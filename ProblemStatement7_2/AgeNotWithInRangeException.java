6af8596156f16ae6e7a121511a6d0c14eaada710
NayanAich <aich.nayan2@gmail.com>
2022-03-18 01:29:38 +0530

    removing duplicates

--- ProblemStatement7_2/AgeNotWithInRangeException.java
+++
@@ -1,8 +0,0 @@
-package ProblemStatement7_2;
-class AgeNotWithInRangeException extends Exception
-{
-     public String toString()
-     {
-          return ("Age is not between 15 and 21. please ReEnter the Age");
-     }
-}
--- ProblemStatement7_2/NameNotValidException.java
+++
@@ -1,9 +0,0 @@
-package ProblemStatement7_2;
-
-class NameNotValidException extends Exception
-{
-     public String validname()
-     {
-          return ("Name is not Valid..Please ReEnter the Name");
-     }
-}
